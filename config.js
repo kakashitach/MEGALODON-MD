da769135ddcd2370e7f96e7eca6ffe30104cff4c
kakashitach <keyoojunie@gmail.com>
2025-07-09 04:15:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ ANTILINK: process.env.ANTILINK || "false",
 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
